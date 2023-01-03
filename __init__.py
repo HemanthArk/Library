ebbb6acddce3da131f70d60ead1c45ad3afae568
HemanthArk <121861333+HemanthArk@users.noreply.github.com>
2023-01-03 15:29:49 +0530

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+copyright (c) 2016  Earth Advantage.
+All rights reserved
+"""
+
+# Local Imports
+
+from normalize import (
+    get_geocoder_normalized_addr,
+    normalize_address_record,
+)
+print("yes")
---
+++ main.py
@@ -0,0 +1,8 @@
+import usaddress
+
+from normalize import normalize_address_record
+a=normalize_address_record("ROAD 5602 JENNIE WEBBER LAKE RHINELANDER WI 54501")
+print(a)
+#a=usaddress.tag("9000 Rockville Pike Bldg 10 Crc B2-2426 Bethesda MD 20892")
+
+#print(a)
